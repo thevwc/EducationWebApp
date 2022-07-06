26636210833bbbc739f3f7e304697fed33e63951
David Neiss <davidaneiss@gmail.com>
2022-07-06 19:09:24 -0400

    Trying to add logging to Education web app.

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,36 @@
 # __init__.py
 
-#import logging
-#from logging.handlers import RotatingFileHandler
-#import os
+import logging
+
+# from logging.handlers import RotatingFileHandler
+# import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import Config
 from flask_mail import Mail, Message
+
+logger = None
+
+
+def InitLogging():
+    """
+    Inits the logging subsystem
+    """
+    LOG_FILE = "education.log"
+
+    global logger
+    logger = logging.getLogger("")
+    logger.setLevel(logging.DEBUG)
+
+    formatString = "%(asctime)s %(module)-4s %(levelname)-7s %(message)s"
+    LoggerFileHandler = logging.FileHandler(LOG_FILE)
+    LoggerFileHandler.setFormatter(logging.Formatter(formatString, "%Y-%m-%d %H:%M:%S"))
+    logger.addHandler(LoggerFileHandler)
+
+
+InitLogging()
+logger.debug("Started Education web app")
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,4 +43,3 @@ db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
-
